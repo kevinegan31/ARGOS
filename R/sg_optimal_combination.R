e63d4ce8693a9eefe1060aca8e36fa5e5bda4359
Kevin Egan <kevinegan31@gmail.com>
2023-04-22 10:05:13 +0100

    Update sg_optimal_combination.R

--- R/sg_optimal_combination.R
+++ R/sg_optimal_combination.R
@@ -4,14 +4,12 @@ sg_optimal_combination <- function(x_t, dt = 1, polyorder) {
   ### Need new name for special_digit
   dt <- dt
   ### Create Combinations
-  ### Window length of odd numbers from [11,101]
   wl_max <- round((nrow(as.matrix(x_t)) * 0.05), 0)
   if (round((wl_max %% 2), 0) == 0) {
     wl_max <- wl_max + 1
   }
-  ### Polynomial Order is 3 - 4
+  ### Polynomial Order
   if (missing(polyorder)) {
-    # polyorder <- seq(4, 5, 6, by = 1)
     polyorder <- 4
   }
   ### If the Window length calculation is less than 11
@@ -39,21 +37,10 @@ sg_optimal_combination <- function(x_t, dt = 1, polyorder) {
     }
   }
   ### Determine Frobenius Norm for Combinations
-  # x_t_smoothed_list <- list()
   f_dist <- list()
   for (i in seq_along(sg_combinations[, 1])) {
     ### Smooth data
-    # x_t_smoothed <- apply(x_t, 2, sgolayfilt,
-    #                       p = sg_combinations[i, 1],
-    #                       n = sg_combinations[i, 2],
-    #                       m = 0, ts = dt) %>%
-    #   as.data.frame()
     x_t_smoothed <- x_t %>% sgolayfilt(p = sg_combinations[i, 1], n = sg_combinations[i, 2], m = 0, ts = dt)
-    # A <- as.matrix(x_t - x_t_smoothed)
-    # A_t <- t(A)
-    # trace_A_t_A <- sum(diag(A_t %*% A))
-    # f_dist[[i]] <- sqrt(trace_A_t_A)
-    # x_t_smoothed_list[[i]] <- x_t_smoothed
     f_dist[[i]] <- Metrics::mse(x_t,
                                 x_t_smoothed)
   }
@@ -64,8 +51,6 @@ sg_optimal_combination <- function(x_t, dt = 1, polyorder) {
   sg_best_combination <- which.min(f_dist_df$f_dist)
   sg_order_wl <- cbind(sg_combinations[which.min(f_dist_df$f_dist), 1],
                        sg_combinations[which.min(f_dist_df$f_dist), 2])
-  # sg_optimal <- x_t_smoothed_list[[sg_best_combination]]
-  # sg_window_length <- sg_combinations[which.min(f_dist_df$f_dist), 2]
   return(list(sg_combinations = sg_combinations,
               sg_order_wl = sg_order_wl,
               f_dist = f_dist_df))
