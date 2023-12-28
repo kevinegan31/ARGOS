4a34846c14d8b5a2b51d46f7b8470e8aeb794aed
Kevin Egan <36718924+kevinegan31@users.noreply.github.com>
2023-12-28 10:43:20 -0600

    Delete tests directory

--- tests/testthat.R
+++
@@ -1,7 +0,0 @@
-# library(ARGOS)
-# devtools::load_all(".")
-# devtools::test()
-# devtools::test("tests/testthat")
-library(testthat)
-library(ARGOS)  # Your package
-test_check("ARGOS")  # Your package name
